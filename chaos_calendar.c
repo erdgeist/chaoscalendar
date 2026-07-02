48fe5c517ae3895cc78d9d2fefbdd35b90d52ba7
erdgeist <erdgeist@erdgeist.org>
2026-07-02 14:20:04 +0200

    Add local tmp variable to take timezone string

--- chaos_calendar.c
+++ chaos_calendar.c
@@ -46,7 +46,7 @@ static VALUE to_time( VALUE input, char * name ) {
 // TODO: https://www.w3.org/Tools/Ical2html/ical2html.c
 
 VALUE occurrences_for_timezone( VALUE self, VALUE dtstart, VALUE dtend, VALUE rrule, VALUE timezone ) {
-  char * _rrule;
+  char * _rrule, *_timezone;
   struct icaltimetype start, end;
   icaltimezone * tz = NULL;
   time_t tt;
